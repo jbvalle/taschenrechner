2efbd68c2dcfe6cc6442e4c5ed161ae7773c2654
John Bryan Valle <jbvalle.jv@gmail.com>
2021-10-06 02:31:30 +0200

    first stable release

---
+++ README.md
@@ -0,0 +1,20 @@
+# calculator_with_flags_arg
+
+Input:
+honey val1 operator1 val2 operator 2 val3 ... etc
+
+Operators are:
++ (addition)
+- (subtraction)
+/ (division)
+x (multiplication)
+power (power)
+root (root)
+
+Features:
+• Arguments in Terminal
+• Endless number of values and operators
+
+Bugs:
+• No operator priotization
+• Calculation only works from left to right
---
+++ honey.c
@@ -0,0 +1,58 @@
+
+#include <stdio.h>
+#include <stdlib.h> //for strtol
+#include <string.h> //for strcmp
+#include <math.h> //Root and Power
+
+int main(int argc, char *argv[]) {
+
+  // string to long(*string, **endpointer, base:2-36)
+  //Converting array of char pointers to double
+  //Reading in first value of arguments
+  double result = (double)strtol(argv[1], NULL, 10);
+
+  //Reading in flags 
+  for (int i = 0; i < argc; i++) {
+
+    //Addition
+    if(strcmp(argv[i],"+") == 0) {
+
+        double arg = (double)strtol(argv[i+1], NULL, 10);
+        result += arg;
+    }
+    //Subtraction
+    if(strcmp(argv[i], "-") == 0) {
+
+        double arg = (double)strtol(argv[i+1], NULL, 10);
+        result -= arg;
+    }
+    //Multiplication
+    if(strcmp(argv[i],"x") == 0) {
+
+        double arg = (double)strtol(argv[i+1], NULL, 10);
+        result = result * arg;
+    }
+    //Division
+    if(strcmp(argv[i],"/") == 0) {
+
+        double arg = (double)strtol(argv[i+1], NULL, 10);
+        result /= arg;
+    }
+    //Power
+    if(strcmp(argv[i],"power") == 0) {
+
+        double arg = (double)strtol(argv[i+1], NULL, 10);
+        result = pow(result, arg);
+    }
+    //Root
+    if(strcmp(argv[i],"root") == 0) {
+
+        double arg = (double)strtol(argv[i+1], NULL, 10);
+        result = pow(result, 1./arg);
+    }
+  }
+  printf("\n\n-------------------------\n");
+  printf("\n\t\t%f\n", result);
+
+  return 0;
+}
