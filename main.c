77673edbbf0ed7a18b78502ddc4948b97202177d
John Bryan Valle <jbvalle.jv@gmail.com>
2021-10-14 17:13:38 +0200

    Solved exiting problem

--- main.c
+++ main.c
@@ -1,3 +1,27 @@
+/*
+  John Bryan Valle
+  Programm: taschenrechner
+
+  + = a
+  - = s
+  * = m
+  / = d
+  ()^x = pow               //bsp: 2 pow 5 = 2^5
+  root_of = xte wurzel von //bsp: 25 root_of 2 = 25^(1/2)
+  % = r
+
+  Eingabe: ------------------------------------------------------------
+  1. Build:
+    ~&: make
+  2. Ausführen:
+    ~&: ./main
+  3. Eingabe:
+    ~&: 3 a 3 m 5 pow 5 r 2
+  4. Beenden:
+    ~&: e
+  ---------------------------------------------------------------------*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,25 +40,25 @@ int main(int argc, char const *argv[]) {
   do{
       //Zeichenweises Einlesen vom String
       for (int i = 0; (letter = getchar()) != '\n'; i++) {
+
         args[i] = letter;
       }
-
       //Bei Eingabe mit e -> Programabbruch
-      if (strcmp(args,"e") == 0){
+      if (strcmp(args,"e") != 0){
+
+        strcat(command, args);
+        //Programm Taschenrechner mit parameter aufrufen
+        system(command);
+        //main befehl wird erneut ausgeführt
+        system("main");
+      }else{
 
         printf("-----Programmende!-----\n");
         break;
       }
 
-      //Parameter an Programm kommando anhängen
-      strcat(command, args);
-      //Programm Taschenrechner mit parameter aufrufen
-      system(command);
-      //main befehl wird erneut ausgeführt
-      system("main");
+  }while(strcmp(args,"e")==0);
 
-
-  }while(1);
 
   return 0;
 }
